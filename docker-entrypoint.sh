ac55234e657c1d634819d556506e8bc36c9eba14
knutole <knutole@mapic.io>
2018-10-02 16:20:52 +0000

    move config; use bridge network

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,13 @@ PORT=8979
 
 export SHINY_LOG_LEVEL=DEBUG
 
+# /opt/shiny-server/bin/license-manager license-server $SERVER:$PORT
+echo "SHINY_LICENSE_SERVER: $SHINY_LICENSE_SERVER"
+echo "SHINY_LICENSE_PORT: $SHINY_LICENSE_PORT"
+
 # configure license manager
-/opt/shiny-server/bin/license-manager license-server $SERVER:$PORT
+# /opt/shiny-server/bin/license-manager license-server $SHINY_LICENSE_SERVER:$SHINY_LICENSE_PORT
+/opt/shiny-server/bin/license-manager license-server localhost:$SHINY_LICENSE_PORT
 
 # start shiny
 shiny-server --pidfile=/var/run/shiny-server.pid 
