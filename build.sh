9c6ab9be2a1c1e1ec6843740165709d5ab29b6f2
knutole <knutole@mapic.io>
2018-10-11 22:39:30 +0000

    verify build

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# build
 docker build -t mapic/shiny-server-pro .
 
+# verify
 if [ "$?" -gt "0" ]; then
     echo "Build errored."
     exit 1
 fi
+
+# push
 docker push mapic/shiny-server-pro:latest
