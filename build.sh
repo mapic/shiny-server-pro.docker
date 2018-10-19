810d123b0ce374c0bec0d042707d148922d6d4f6
knutole <knutole@mapic.io>
2018-10-19 22:19:13 +0000

    remove push from build

--- build.sh
+++ build.sh
@@ -2,12 +2,3 @@
 
 # build
 docker build -t mapic/shiny-server-pro .
-
-# verify
-if [ "$?" -gt "0" ]; then
-    echo "Build errored."
-    exit 1
-fi
-
-# push
-docker push mapic/shiny-server-pro:latest
