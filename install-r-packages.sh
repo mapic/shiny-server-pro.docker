3cc5ec18b7933b6b29c8b6812194d5061f7cebf5
knutole <knutole@mapic.io>
2018-10-19 22:28:48 +0000

    cleanup

--- install-r-packages.sh
+++ install-r-packages.sh
@@ -4,6 +4,7 @@ while read F  ; do
     
     # install package
     echo "Installing $F..."
+    # https://stackoverflow.com/questions/25721884/how-should-i-deal-with-package-xxx-is-not-available-for-r-version-x-y-z-wa
     # sudo su - -c "R -e \"install.packages(c('${F}'), repos='https://cran.rstudio.com/')\""
     sudo su - -c "R -e \"withCallingHandlers(install.packages(c('${F}'), repos='https://cran.rstudio.com/'), warning = function(w) stop(w))\""
     
